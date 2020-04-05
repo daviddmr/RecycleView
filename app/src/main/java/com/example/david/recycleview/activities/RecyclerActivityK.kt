fb4a0a85968bf21299e15b20b5b25f3020295a95
daviddmr <david.dmr@gmail.com>
2020-04-05 18:25:46 -0400

    Set up recycler view in RecyclerActivityK

--- app/src/main/java/com/example/david/recycleview/activities/RecyclerActivityK.kt
+++ app/src/main/java/com/example/david/recycleview/activities/RecyclerActivityK.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.david.recycleview.R
+import com.example.david.recycleview.adapters.MyAdapter
 import com.example.david.recycleview.viewModels.RecyclerViewModel
 import kotlinx.android.synthetic.main.activity_recycler_view.*
 
@@ -13,14 +14,17 @@ class RecyclerActivityK : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
+        val viewModel = ViewModelProvider(this).get(RecyclerViewModel::class.java)
 
-        setupViewComponents()
-    }
-
-    private fun setupViewComponents() {
         rv_main_list.setHasFixedSize(true)
+        rv_main_list.addOnScrollListener(viewModel.onScrollListener())
         val mLinearLayoutManager = LinearLayoutManager(this)
         mLinearLayoutManager.orientation = LinearLayoutManager.VERTICAL
         rv_main_list.layoutManager = mLinearLayoutManager
+
+        val mList = viewModel.getRandomList(10)
+
+        val myAdapter = MyAdapter(this, mList)
+        rv_main_list.adapter = myAdapter
     }
 }
